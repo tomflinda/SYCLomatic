acaef1b9efbc1973e9f14bc1243f3a1cdfb817f1
chenwei.sun <chenwei.sun@intel.com>
2025-05-13 14:08:39 +0800

    Further update
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/tools/dpct/generate_api_migration_status.py
+++ clang/tools/dpct/generate_api_migration_status.py
@@ -174,6 +174,9 @@ def do_update(args):
         return False
     lib_names = ['Runtime_and_Driver', 'CUB', 'cuBLAS', 'cuDNN', 'cuFFT', 'nvGRAPH', 'ASM',
                  'cuRAND', 'cuSOLVER', 'cuSPARSE', 'NCCL', 'nvJPEG', 'NVML', 'thrust', 'wmma']
+
+
+
     # lib file name = APINames_$(libname).inc
     for lib_name in lib_names:
         if lib_name == 'Runtime_and_Driver':
