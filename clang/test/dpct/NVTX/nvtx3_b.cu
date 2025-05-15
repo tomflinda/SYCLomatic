3a49f99b877e210862c2c3b381b7aa28be847e13
chenwei.sun <chenwei.sun@intel.com>
2025-05-15 10:50:57 +0800

    udpate nvtx3_b.cu
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/NVTX/nvtx3_b.cu
+++ clang/test/dpct/NVTX/nvtx3_b.cu
@@ -1,6 +1,6 @@
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2
-// RUN: dpct --format-range=none -in-root %S -out-root %T %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
-// RUN: FileCheck --input-file %T/nvtx3_b.dp.cpp --match-full-lines %s
+// RUN: dpct --format-range=none -in-root %S -out-root %T/nvtx3_b %s --cuda-include-path="%cuda-path/include" -- -std=c++14 -x cuda --cuda-host-only
+// RUN: FileCheck --input-file %T/nvtx3_b/nvtx3_b.dp.cpp --match-full-lines %s
 
 // CHECK: #include <sycl/sycl.hpp>
 // CHECK: #include <dpct/dpct.hpp>
