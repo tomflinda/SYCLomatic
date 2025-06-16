24ce8666d50ff8c3bcb2d152250e44952e3b4395
chenwei.sun <chenwei.sun@intel.com>
2025-06-16 09:01:42 +0800

    Add thrust::log10
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
+++ clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
@@ -3,3 +3,5 @@
 // thrust_abs:  std::abs(std::complex<float>(0.0));
 
 
+// RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::log10 --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_log10
+// thrust_log10:  std::log10(std::complex<float>(0.0));
