8dc7d4c598b5553c39f0ef400c1e0b068cce0b68
chenwei.sun <chenwei.sun@intel.com>
2025-06-16 09:21:35 +0800

    thrust::sqrt added
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
+++ clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
@@ -2,6 +2,9 @@
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::abs --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_abs
 // thrust_abs:  std::abs(std::complex<float>(0.0));
 
-
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::log10 --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_log10
 // thrust_log10:  std::log10(std::complex<float>(0.0));
+
+// RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::sqrt --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_sqrt
+// thrust_sqrt:  std::sqrt(std::complex<float>(0.0));
+
