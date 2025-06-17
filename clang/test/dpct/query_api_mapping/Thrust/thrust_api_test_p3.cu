e7f87518a941e9fb9eb6be21a4c7236d696f03b8
chenwei.sun <chenwei.sun@intel.com>
2025-06-17 14:32:17 +0800

    address review comments
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
+++ clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p3.cu
@@ -1,5 +1,5 @@
 
-// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, V10.1
+// UNSUPPORTED: v8.0, v9.0, v9.1, v9.2, v10.0, v10.1
 // UNSUPPORTED: cuda-8.0, cuda-9.0, cuda-9.1, cuda-9.2, cuda-10.0, cuda-10.1
 // RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::abs --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_abs
 // thrust_abs:CUDA API:
