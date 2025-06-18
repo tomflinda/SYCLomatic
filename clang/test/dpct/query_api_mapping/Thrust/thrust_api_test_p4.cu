6b9e81f0b9f0e6aa23fe454638cf06703a2cbe10
chenwei.sun <chenwei.sun@intel.com>
2025-06-18 13:17:49 +0800

    any_of
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p4.cu
+++ clang/test/dpct/query_api_mapping/Thrust/thrust_api_test_p4.cu
@@ -20,3 +20,25 @@
 // thrust_adjacent_difference-NEXT:  /*2*/ oneapi::dpl::adjacent_difference(oneapi::dpl::execution::seq, host_ptr_A, host_ptr_A+10, host_ptr_R, std::minus<float>());
 // thrust_adjacent_difference-NEXT:  /*3*/ oneapi::dpl::adjacent_difference(oneapi::dpl::execution::make_device_policy(q_ct1), device_ptr_A, device_ptr_A+10, device_ptr_R, std::minus<float>());
 // thrust_adjacent_difference-NEXT:  /*4*/ oneapi::dpl::adjacent_difference(oneapi::dpl::execution::seq, host_ptr_A, host_ptr_A+10, host_ptr_R);
+
+// RUN: dpct --cuda-include-path="%cuda-path/include" --query-api-mapping=thrust::any_of --extra-arg="-std=c++14"| FileCheck %s -check-prefix=thrust_any_of
+// thrust_any_of: CUDA API:
+// thrust_any_of-NEXT:  struct greater_than_zero {
+// thrust_any_of-NEXT:    __host__ __device__ bool operator()(int x) const { return x > 0; }
+// thrust_any_of-NEXT:  };
+// thrust_any_of-NEXT:  greater_than_zero pred;
+// thrust_any_of-NEXT:  thrust::device_vector<int> A(4);
+// thrust_any_of-NEXT:  thrust::device_vector<int> B(4);
+// thrust_any_of-NEXT:  /*1*/ thrust::any_of(A.begin(), A.end(), pred);
+// thrust_any_of-NEXT:  /*2*/ thrust::any_of(thrust::device, B.begin(), B.end(), pred);
+// thrust_any_of-NEXT:Is migrated to:
+// thrust_any_of-NEXT:  struct greater_than_zero {
+// thrust_any_of-NEXT:    bool operator()(int x) const { return x > 0; }
+// thrust_any_of-NEXT:  };
+// thrust_any_of-NEXT:  greater_than_zero pred;
+// thrust_any_of-NEXT:  dpct::device_vector<int> A(4);
+// thrust_any_of-NEXT:  dpct::device_vector<int> B(4);
+// thrust_any_of-NEXT:  /*1*/ oneapi::dpl::any_of(oneapi::dpl::execution::make_device_policy(q_ct1), A.begin(), A.end(), pred);
+// thrust_any_of-NEXT:  /*2*/ oneapi::dpl::any_of(oneapi::dpl::execution::make_device_policy(q_ct1), B.begin(), B.end(), pred);
+
+
