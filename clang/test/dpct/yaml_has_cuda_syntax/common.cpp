5ea41a8f5bc56e6f1dbb9639ce7bf4bad84decad
chenwei.sun <chenwei.sun@intel.com>
2025-06-30 11:12:23 +0800

    fix CI failure
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- clang/test/dpct/yaml_has_cuda_syntax/common.cpp
+++ clang/test/dpct/yaml_has_cuda_syntax/common.cpp
@@ -17,7 +17,7 @@
 // RUN: %if build_lit %{icpx -c -fsycl %T/out/common.cpp -o %T/out/common.o %}
 
 // RUN: FileCheck %S/utils.cpp --match-full-lines --input-file %T/out/utils.cpp.dp.cpp
-// RUN: %if build_lit %{icpx -c -fsycl %T/out/utils.cpp.dp.cpp -o %T/out/ utils.cpp.dp.o %}
+// RUN: %if build_lit %{icpx -c -fsycl %T/out/utils.cpp.dp.cpp -o %T/out/utils.cpp.dp.o %}
 
 // RUN: cat %S/MainSourceFiles.ref > %T/out/check_MainSourceFiles.yaml
 // RUN: cat %T/out/MainSourceFiles.yaml >>%T/out/check_MainSourceFiles.yaml
